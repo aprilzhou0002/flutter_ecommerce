c14751d7564938c92da718a817037bd1bdd5f23d
Aprilm <keitrymairel638@gmail.com>
2024-05-05 10:32:54 -0700

    Starter Code

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/theme/theme.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+    );
+  }
+}
