fa09326bba21b1fd0ca23d9ee975483388cac3de
Aprilm <keitrymairel638@gmail.com>
2024-05-05 12:00:50 -0700

    first-change

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class TColors {
+  TColors._();
+
   // App theme colors
   static const Color primary = Color(0xFF4b68ff);
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
+
+  // Gradient colors
+  static const Gradient linerGradient = LinearGradient(
+    begin:Alignment(0.0, 0.0),
+    end: Alignment(0.707, -0.707),
+    colors: [
+      Color(0xffff9a9e),
+      Color(0xfffad0c4),
+      Color(0xfffad0c4),
+  ]);
 
   // Text colors
   static const Color textPrimary = Color(0xFF333333);
