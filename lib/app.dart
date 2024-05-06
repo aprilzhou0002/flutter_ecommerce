99bf91be2c93ef56b9665118927f6d58f68c6947
Aprilm <keitrymairel638@gmail.com>
2024-05-05 23:32:58 -0700

    login

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:t_store/features/authentication/screens/onboarding/onboarding.dart';
-import 'package:t_store/utils/constants/text_strings.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
---
+++ lib/features/authentication/screens/login/login.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_utils/get_utils.dart';
+import 'package:t_store/features/authentication/screens/login/widgets/login_form.dart';
+import 'package:t_store/features/authentication/screens/login/widgets/login_header.dart';
+
+import '../../../../common/styles/spacing_styles.dart';
+import '../../../../common/widgets/login_signup/form_divider.dart';
+import '../../../../common/widgets/login_signup/social_buttons.dart';
+import '../../../../utils/constants/sizes.dart';
+import '../../../../utils/constants/text_strings.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // final dark = THelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              // Logo, Title & Sub-Title
+              const TLoginHeader(),
+
+              // Form
+              const TLoginForm(),
+
+              // Divider
+              TFormDivider(dividerText: TTexts.orSignInWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              // Footer
+              TSocialButtons()
+            ],
+          )
+        )
+      )
+    );
+  }
+}
+
+
+
+
+
+
+
+
