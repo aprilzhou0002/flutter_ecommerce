2a7e5b54f6f27c63c1894438bf724cf4a17f5c85
Aprilm <keitrymairel638@gmail.com>
2024-05-05 22:02:28 -0700

    onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/theme/theme.dart';
+import 'package:t_store/app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -9,16 +9,6 @@ class TColors {
   static const Color secondary = Color(0xFFFFE24B);
   static const Color accent = Color(0xFFb0c7ff);
 
-  // Gradient colors
-  static const Gradient linerGradient = LinearGradient(
-    begin:Alignment(0.0, 0.0),
-    end: Alignment(0.707, -0.707),
-    colors: [
-      Color(0xffff9a9e),
-      Color(0xfffad0c4),
-      Color(0xfffad0c4),
-  ]);
-
   // Text colors
   static const Color textPrimary = Color(0xFF333333);
   static const Color textSecondary = Color(0xFF6C757D);
